d8c7b3b54640d91c8e650f93c8359595e5b688cf
Adrien BLANCHOT <caboteur.adrienbanchot@gmail.com>
2020-04-23 19:46:42 +0200

    top

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -12,40 +12,46 @@ class Viewer extends Component {
   render() {
     return (
       <div id={this.props.id} className={styles.articles} style={{display:this.props.display}}>
-        <div className={styles.headerContainer}>
-       <img alt="exitButton" className={styles.exitButton} src={exit} onClick={this.props.click}/>
-        <h1 className={styles.title}>{this.props.topics}</h1>
-        </div>
-     <div>
-       <a href={this.props.wikiurl}>
-       <img alt="wikipedia" className={styles.icon} src={wikipedia} />
+      <div className={styles.headerContainer}>
+     <img alt="exitButton" className={styles.exitButton} src={exit} onClick={this.props.click}/>
+      <h1 className={styles.title}>{this.props.topics}</h1>
+      </div>
+   <div>
+     <a href={this.props.wikiurl}>
+       Une page associée sur wikipédia
+     <img alt="wikipedia" className={styles.icon} src={wikipedia} />
 
-       </a>
-     </div>
-     <div>
-       <a href={this.props.googleSearch}>
-       <img alt="google" className={styles.icon} src={google} />
+     </a>
+   </div>
+   <div>
+     <a href={this.props.googleSearch}>
+        Recherchez ce therme sur google
+     <img alt="google" className={styles.icon} src={google} />
 
-       </a>
-     </div>
-     <div>
-       <a href={this.props.twitterSearch}>
-       <img alt="twitter" className={styles.icon} src={twitter} />
+     </a>
+   </div>
+   <div>
+     <a href={this.props.twitterSearch}>
+         Recherchez ce therme sur twitter
+     <img alt="twitter" className={styles.icon} src={twitter} />
 
-       </a>
-     </div>
-     <div><a href={this.props.youtubeSearch}>
-       <img alt="youtube" className={styles.icon} src={youtube} />
+     </a>
+   </div>
+   <div><a href={this.props.youtubeSearch}>
+      Recherchez ce therme sur youtube
+     <img alt="youtube" className={styles.icon} src={youtube} />
+  >
+     </a>
+   </div>
+   <div>
+     <a href={this.props.url}>
+       Un article associé
+     <img alt="news" className={styles.icon} src={news} />
+     {this.props.url?   <div><h2 className={styles.article}> {this.props.title}</h2>
+   <p>{this.props.url.source} </p> </div> : <h2></h2> }
+     </a>
+   </div>
 
-       </a>
-     </div>
-     <div>
-       <a href={this.props.url}>
-       <img alt="news" className={styles.icon} src={news} />
-       {this.props.url?   <div><h2 className={styles.article}> {this.props.source}</h2>
-     <p>{this.props.source} </p> </div> : <h2></h2> }
-       </a>
-     </div>
 
 
 
